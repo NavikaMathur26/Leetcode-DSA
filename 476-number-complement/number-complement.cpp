4453f3c940ac71ff9e93fc027b789ab5eb379e5b
NavikaMathur26 <navikamathur26@gmail.com>
2026-01-03 11:14:51 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (15.02%) - LeetSync

---
+++ 476-number-complement/number-complement.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int i=0,sum=0;
+        while(num>0){
+            int rem=num%2;
+            if(rem==0){
+               sum+=(pow(2,i));
+               i++;}
+            else
+              i++;
+            num/=2;     
+        }
+        return sum;
+    }
+};
