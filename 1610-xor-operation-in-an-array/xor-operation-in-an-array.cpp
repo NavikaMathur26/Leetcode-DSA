87541e3af392d7f1f8a00c86e93a8eac1119301e
NavikaMathur26 <navikamathur26@gmail.com>
2026-01-06 22:39:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (15.92%) - LeetSync

---
+++ 1610-xor-operation-in-an-array/xor-operation-in-an-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        vector<int> nums;
+        int res=0;
+        for(int i=0;i<n;i++){
+            nums.push_back(start+2*i);
+            res=res^nums[i];
+        }
+        return res;
+    }
+};
