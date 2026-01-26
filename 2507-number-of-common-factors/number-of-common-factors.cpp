098b62e7fcfb65bc7fcbc2d9d3b8a6ae510929d2
NavikaMathur26 <navikamathur26@gmail.com>
2026-01-26 17:08:45 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (44.93%) - LeetSync

--- 2507-number-of-common-factors/number-of-common-factors.cpp
+++ 2507-number-of-common-factors/number-of-common-factors.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
     int commonFactors(int a, int b) {
-        vector<int> v;
-        int n=min(a,b);
-        for(int i=1;i<=n;i++){
-            if(n%i==0)
-              v.push_back(i);
+        int counter=0;
+        for(int i=1;i<=min(a,b);i++){
+            if(a%i==0 && b%i==0)
+               counter++;
         }
-        int m=max(a,b);
-        int count=0;
-        for(int i=0;i<v.size();i++){
-            if(m%v[i]==0)
-              count++;
-        }
-        return count;
+        return counter;
     }
 };
